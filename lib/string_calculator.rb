fe6cbaba2e4e68375a0ccd0dc36dc972f83eb23a
Rishav Kumar <rishav74492644@gmail.com>
2024-05-18 22:33:54 +0530

    Optimize StringCalculator class and its methods - Create private helper methods 'get_digits' and 'check_for_negative_numbers'  to encapsulate logic and improve maintainability. Convert add method to instance method. Update test to use instance of StringCalculator.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,25 +2,35 @@
 
 # String calculator with a add method that accepts string
 class StringCalculator
-  def self.add(numbers)
+  def add(numbers)
     return 0 if numbers.nil?
 
-    delimiter = /,|\n/
+    digits = get_digits(numbers)
 
+    check_for_negative_numbers(digits)
+
+    return 0 if digits.empty?
+
+    digits.reduce(0, :+)
+  end
+
+  private
+
+  def get_digits(numbers)
     if numbers.start_with? '/'
       line_break_split = numbers.split("\n", 2)
       delimiter = Regexp.escape(line_break_split.first[2])
       numbers = line_break_split.last
+    else
+      delimiter = /,|\n/
     end
 
-    nums = numbers.split(delimiter).map(&:to_i)
-    negatives = nums.select(&:negative?)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def check_for_negative_numbers(numbers)
+    negatives = numbers.select(&:negative?)
 
     raise "Negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
-
-    return numbers.to_i if numbers.split(delimiter).count < 2
-
-    digits = numbers.split(delimiter).map(&:to_i)
-    digits.reduce(0, :+)
   end
 end
