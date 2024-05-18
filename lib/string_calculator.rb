bbe1efe31de5dde5d6fabf6b38d3c5983cc5edb7
Rishav Kumar <rishav74492644@gmail.com>
2024-05-18 21:52:30 +0530

    Add support for different delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,7 +5,13 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.nil?
 
-    delimiter = /,|\n|;/
+    delimiter = /,|\n/
+
+    if numbers.start_with? '/'
+      line_break_split = numbers.split("\n", 2)
+      delimiter = Regexp.escape(line_break_split.first[2])
+      numbers = line_break_split.last
+    end
 
     return numbers.to_i if numbers.split(delimiter).count < 2
 
