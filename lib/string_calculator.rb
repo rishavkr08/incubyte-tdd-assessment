bdac11cf2a4c101601b22fcb5bb9b78765f4c584
Rishav Kumar <rishav74492644@gmail.com>
2024-05-18 21:32:06 +0530

    Add multiple dilimeter usage. Fix rubocop offense.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
+# String calculator with a add method that accepts string
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty?
-    return numbers.to_i unless numbers.include? ","
-    
-    digits = numbers.split(",").map(&:to_i)
+    return 0 if numbers.nil?
+
+    delimiter = /,|\n|;/
+
+    return numbers.to_i if numbers.split(delimiter).count < 2
+
+    digits = numbers.split(delimiter).map(&:to_i)
     digits.reduce(0, :+)
-  end  
+  end
 end
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -1,28 +1,32 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative '../lib/string_calculator'
 
 class StringCalculatorTest < Minitest::Test
+  def test_add_nil_string
+    assert_equal 0, StringCalculator.add(nil)
+  end
+
   def test_add_empty_string
-    assert_equal 0, StringCalculator.add("")
+    assert_equal 0, StringCalculator.add('')
   end
 
   def test_add_single_number
-    assert_equal 10, StringCalculator.add("10")
+    assert_equal 10, StringCalculator.add('10')
   end
 
   def test_add_two_numbers
-    assert_equal 30, StringCalculator.add("10, 20")
+    assert_equal 30, StringCalculator.add('10, 20')
   end
 
   def test_add_any_numbers
-    assert_equal 100, StringCalculator.add("10, 20, 30, 20, 5, 15")
+    assert_equal 100, StringCalculator.add('10, 20, 30, 20, 5, 15')
   end
 
   def test_add_any_numbers_with_other_delimeter
-    assert_equal ["10\n 20", " 30"], "10\n 20, 30".split(",")
-    assert_equal [10, 30], "10\n 20, 30".split(",").map(&:to_i)
-
-    refute_equal 60, StringCalculator.add("10\n 20, 30")
-    assert_equal 40, StringCalculator.add("10\n 20, 30")
+    assert_equal 60, StringCalculator.add('10, 20, 30')
+    assert_equal 60, StringCalculator.add("10 \n20, 30")
+    assert_equal 60, StringCalculator.add('10; 20, 30')
   end
 end
