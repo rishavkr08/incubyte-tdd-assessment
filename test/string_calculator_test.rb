c21ca01e963d224cdc671152893b565e4c44294e
Rishav Kumar <rishav74492644@gmail.com>
2024-05-18 20:50:22 +0530

    Add test for line break delimeter and data missing.

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -17,4 +17,12 @@ class StringCalculatorTest < Minitest::Test
   def test_add_any_numbers
     assert_equal 100, StringCalculator.add("10, 20, 30, 20, 5, 15")
   end
+
+  def test_add_any_numbers_with_other_delimeter
+    assert_equal ["10\n 20", " 30"], "10\n 20, 30".split(",")
+    assert_equal [10, 30], "10\n 20, 30".split(",").map(&:to_i)
+
+    refute_equal 60, StringCalculator.add("10\n 20, 30")
+    assert_equal 40, StringCalculator.add("10\n 20, 30")
+  end
 end
