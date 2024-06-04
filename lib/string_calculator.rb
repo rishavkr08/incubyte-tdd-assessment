6357c8a3255737f739f19ec8734d08f512414573
Rishav Kumar <rishav74492644@gmail.com>
2024-06-04 17:32:45 +0530

    Add handler for operators delimiter and perform operations.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,7 @@
 # String calculator with an add method that accepts a string
 class StringCalculator
   def add(numbers)
-    return 0 if numbers.nil?
+    return 0 if numbers.nil? || numbers == ''
 
     check_for_invalid_input(numbers)
 
@@ -11,21 +11,28 @@ class StringCalculator
 
     check_for_negative_numbers(digits)
 
-    digits.sum { |d| d < 1000 ? d : 0 }
+    perform_operation(numbers, digits)
   end
 
   private
+
+  def operators
+    %w[+ * - /]
+  end
 
   def get_digits(numbers)
     delimiter = get_delimiters(numbers)
     numbers = numbers.split("\n", 2).last if numbers.start_with?('//')
 
+    delimiter = "\\#{delimiter}" if operators.include? delimiter
     numbers.split(/#{delimiter}/).map(&:to_i)
   end
 
   def get_delimiters(numbers)
     delimiters = /,|\n/
     if numbers.start_with?('//')
+      return numbers.split("\n").first[2..] if numbers.split("\n").first[2..].size == 1
+
       delimiters = numbers[2..].scan(/\[([^\[\]]+)\]/).flatten.map { |d| Regexp.escape(d) }.join('|')
     end
     delimiters
@@ -39,5 +46,23 @@ class StringCalculator
     negatives = numbers.select(&:negative?)
 
     raise "Negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+  end
+
+  def operations
+    {
+      '+' => ->(total, d) { total + d },
+      '*' => ->(total, d) { total * d },
+      '-' => ->(total, d) { total - d },
+      '/' => ->(total, d) { total / d }
+    }
+  end
+
+  def perform_operation(numbers, digits)
+    delimiter = get_delimiters(numbers)
+    if operators.include? delimiter
+      digits[1..].reduce(digits[0], &operations[delimiter])
+    else
+      digits.reduce(0) { |total, d| total + (d < 1000 ? d : 0) }
+    end
   end
 end
